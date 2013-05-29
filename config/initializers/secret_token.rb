f3266a8d94b64b0933cb58d48919b02091b09105
Angelo and Scott <Angelo+Scott@appacademy.io>
2013-05-29 09:21:15 -0400

    foundation setup, initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GistApp::Application.config.secret_token = '5304d62bc5af59c186167a849a65687cba0017bc20436b81d7bbf1c6d8396263a2d515c00b0a1e80c05b4ae394ed71c4d097622e09198ac97d5a12acfd3edebf'
